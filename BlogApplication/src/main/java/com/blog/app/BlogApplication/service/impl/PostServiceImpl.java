bb69c8e631bb71e4a680ad3aea0f29ee880f54af
niran <niran@BOOK-20R96J7JHO>
2024-05-27 14:10:46 +0530

    Commit connect join

--- BlogApplication/src/main/java/com/blog/app/BlogApplication/service/impl/PostServiceImpl.java
+++ BlogApplication/src/main/java/com/blog/app/BlogApplication/service/impl/PostServiceImpl.java
@@ -46,33 +46,6 @@ public class PostServiceImpl implements PostService {
         return postResponse;
     }
 
-    @Override
-    public PostResponse getAllPosts(int pageNo, int pageSize, String sortBy, String sortDir) {
-
-        Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortBy).ascending()
-                : Sort.by(sortBy).descending();
-
-        // create Pageable instance
-        Pageable pageable = PageRequest.of(pageNo, pageSize, sort);
-
-        Page<Post> posts = postRepository.findAll((Example<S>) pageable);
-
-        // get content for page object
-        List<Post> listOfPosts = posts.getContent();
-
-        List<PostDto> content= listOfPosts.stream().map(post -> mapToDTO(post)).collect(Collectors.toList());
-
-        PostResponse postResponse = new PostResponse();
-        postResponse.setContent(content);
-        postResponse.setPageNo(posts.getNumber());
-        postResponse.setPageSize(posts.getSize());
-        postResponse.setTotalElements(posts.getTotalElements());
-        postResponse.setTotalPages(posts.getTotalPages());
-        postResponse.setLast(posts.isLast());
-
-        return postResponse;
-
-    }
 
     @Override
     public PostDto getPostById(long id) {
@@ -100,13 +73,7 @@ public class PostServiceImpl implements PostService {
         postRepository.delete(post);
     }
 
-    @Override
-    public List<PostDto> getPostsByCategory(Long categoryId) {
-        List<Post> posts = postRepository.findByCategoryId(categoryId);
 
-        return posts.stream().map((post) -> mapToDTO(post))
-                .collect(Collectors.toList());
-    }
 
     // convert Entity into DTO
     private PostDto mapToDTO(Post post){
@@ -128,4 +95,16 @@ public class PostServiceImpl implements PostService {
 //        post.setContent(postDto.getContent());
         return post;
     }
+
+	@Override
+	public PostResponse getAllPosts(int pageNo, int pageSize, String sortBy, String sortDir) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<PostDto> getPostsByCategory(Long categoryId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
